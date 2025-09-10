0dac769f6fa697dd70c710ef56cc3a8d35662a16
Vishnuprakash23 <167863222+Vishnuprakash23@users.noreply.github.com>
2025-09-10 22:16:28 +0530

    user entity changes

--- src/main/java/com/spring/EventManagementSystem/controller/UserController.java
+++ src/main/java/com/spring/EventManagementSystem/controller/UserController.java
@@ -1,13 +1,32 @@
 package com.spring.EventManagementSystem.controller;
 
+import com.spring.EventManagementSystem.entity.User;
+import com.spring.EventManagementSystem.service.UserServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/users/{id}")
-    public void displayUserById(){
+    @Autowired
+    private UserServiceLayer myServiceLayer;
+
+    @GetMapping("/users")
+    public String displayUserById(){
+
+        List<User> userList = myServiceLayer.find();
+        System.out.println(userList.get(0));
+//        for(User tempUser : userList){
+//            System.out.println("User name"+ tempUser.getName());
+//            System.out.println("email"+ tempUser.getEmail());
+//            System.out.println("phone number"+ tempUser.getPhoneNumber());
+//            System.out.println("id"+ tempUser.getId());
+//        }
+
+        return "users added and displayed";
 
     }
 
--- src/main/java/com/spring/EventManagementSystem/dao/UserJPARepository.java
+++ src/main/java/com/spring/EventManagementSystem/dao/UserJPARepository.java
@@ -2,7 +2,9 @@ package com.spring.EventManagementSystem.dao;
 
 import com.spring.EventManagementSystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserJPARepository extends JpaRepository<User, Integer> {
 
 }
