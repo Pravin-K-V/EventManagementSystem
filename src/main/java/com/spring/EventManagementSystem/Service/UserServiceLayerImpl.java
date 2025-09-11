34571dd9aac4e9fb61f5775a2a7e790877365568
Vishnuprakash23 <167863222+Vishnuprakash23@users.noreply.github.com>
2025-09-11 23:15:09 +0530

    user service layer updated

--- src/main/java/com/spring/EventManagementSystem/Service/UserServiceLayerImpl.java
+++ src/main/java/com/spring/EventManagementSystem/Service/UserServiceLayerImpl.java
@@ -1,13 +1,25 @@
 package com.spring.EventManagementSystem.service;
 
+import com.spring.EventManagementSystem.component.CSVDataLoader;
 import com.spring.EventManagementSystem.dao.UserJPARepository;
 import com.spring.EventManagementSystem.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class UserServiceLayerImpl implements UserServiceLayer{
 
+    private final CSVDataLoader csvDataLoader;
     public UserJPARepository myUserJPARepository;
+
+    @Autowired
+    public UserServiceLayerImpl(CSVDataLoader csvDataLoader,UserJPARepository userJPARepository) {
+        this.csvDataLoader = csvDataLoader;
+        this.myUserJPARepository = userJPARepository;
+    }
 
     @Override
     public List<User> find(){
@@ -16,7 +28,14 @@ public class UserServiceLayerImpl implements UserServiceLayer{
 
     @Override
     public User findById(int id){
-        return myUserJPARepository.findById(id);
+
+        Optional<User> userList = myUserJPARepository.findById(id);
+        if(userList.isPresent()){
+            return userList.get();
+        }
+        else{
+            throw new RuntimeException("User is not found for the given user id: "+id);
+        }
     }
 
 }
