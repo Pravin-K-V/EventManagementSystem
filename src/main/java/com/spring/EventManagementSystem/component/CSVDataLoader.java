cfcd3b9e6e7644e93dc3383eb56b10a45f6135ce
Vishnuprakash23 <167863222+Vishnuprakash23@users.noreply.github.com>
2025-09-25 23:48:16 +0530

    loading csv data

--- src/main/java/com/spring/EventManagementSystem/component/CSVDataLoader.java
+++ src/main/java/com/spring/EventManagementSystem/component/CSVDataLoader.java
@@ -1,4 +1,22 @@
 package com.spring.EventManagementSystem.component;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CSVDataLoader {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @PostConstruct
+    public void loadCSVData(){
+
+        String sql = "INSERT INTO users(name, email, password, phone_number, role, active)" + "SELECT * FROM CSVREAD('src/main/resources/data.csv')";
+        jdbcTemplate.execute(sql);
+
+    }
+
 }
