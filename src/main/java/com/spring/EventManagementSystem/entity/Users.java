b56a490e64f53b5df645f3c5bc3408d7ce2a9a2e
Vishnuprakash23 <167863222+Vishnuprakash23@users.noreply.github.com>
2025-09-26 23:07:15 +0530

    my entire changes in local done

---
+++ src/main/java/com/spring/EventManagementSystem/entity/Users.java
@@ -0,0 +1,27 @@
+package com.spring.EventManagementSystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String role;
+
+}
